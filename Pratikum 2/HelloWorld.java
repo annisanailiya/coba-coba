9739d92b22e32e7ed86b558f59e3bb99453458a2
fadilaekanoperta <45037793+fadilaekanoperta@users.noreply.github.com>
2021-09-27 10:53:14 +0700

    HelloWorld.java

---
+++ Pratikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Annisa Nailiya Zahroh");
+		System.out.println("NIM  : 6706213136");
+		System.out.println("Kelas: 45-03");
+	}
+}
